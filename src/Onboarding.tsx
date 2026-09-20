beb272e069f0ad620ccee349719260c5593732a7
bradyt88 <bradythomas0936@gmail.com>
2026-09-20 03:27:26 +0100

    Remove unused onboarding useRef import

--- src/Onboarding.tsx
+++ src/Onboarding.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import logoUrl from '../design/brand/family-circle-logo.png'
 import './onboarding.css'
 
